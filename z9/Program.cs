f09cf84dc57bc89239e0098ff2371edd3d27f6be
Mikalai Lutonin <m.lutonin@gmail.com>
2022-10-14 01:59:43 +0300

    Задача 66

--- z9/Program.cs
+++ z9/Program.cs
@@ -1,45 +1,45 @@
-﻿// =============== Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1"
-// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
-
-Console.Clear();
-Console.Write("Введите число N: ");
-int N = int.Parse(Console.ReadLine()!);
-Console.WriteLine(Recurs(N, 1));
-
-string Recurs(int n, int minValue)
-{
-    if (n == minValue)
-    {
-        return n.ToString();               //преобразование int в string
-    }
-    return (n + "," + Recurs(n - 1, minValue));
-}
-
-
-
-
-
-// // =================== Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// // M = 1; N = 15 -> 120
-// // M = 4; N = 8. -> 30
-
+﻿// // =============== Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// // N = 5 -> "5, 4, 3, 2, 1"
+// // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 // Console.Clear();
-// Console.Write("Введите значение M: ");
-// int M = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите значение N: ");
+// Console.Write("Введите число N: ");
 // int N = int.Parse(Console.ReadLine()!);
-// Console.WriteLine(SumRecurs(M,N));
+// Console.WriteLine(Recurs(N, 1));
 
-// int SumRecurs(int m, int n)
+// string Recurs(int n, int minValue)
 // {
-//     if (m == n)
+//     if (n == minValue)
 //     {
-//         return m;
+//         return n.ToString();               //преобразование int в string
 //     }
-//     return (m + SumRecurs(m + 1, n));
+//     return (n + "," + Recurs(n - 1, minValue));
 // }
+
+
+
+
+
+// =================== Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumRecurs(M,N));
+
+int SumRecurs(int m, int n)
+{
+    if (m == n)
+    {
+        return m;
+    }
+    return (m + SumRecurs(m + 1, n));
+}
 
 
 
